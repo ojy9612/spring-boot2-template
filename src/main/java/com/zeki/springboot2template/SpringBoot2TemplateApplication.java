d426bfe58cf28bd8d4b663cf83818c79fb2d26ea
“ojy9612” <ojy9612@gmail.com>
2023-08-31 17:29:47 +0900

    init

---
+++ src/main/java/com/zeki/springboot2template/SpringBoot2TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.zeki.springboot2template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2TemplateApplication.class, args);
+    }
+
+}
