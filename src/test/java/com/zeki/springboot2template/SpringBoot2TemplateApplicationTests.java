96f67d3210f4ad90e70e8cd9e1ca20bb6e01ae96
ojy9612 <ojy9612@gmail.com>
2023-08-31 17:34:46 +0900

    init

---
+++ src/test/java/com/zeki/springboot2template/SpringBoot2TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeki.springboot2template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
